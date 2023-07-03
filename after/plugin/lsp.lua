175be44e341a27c6cfb73ce5b80c92e52a910488
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-07-04 01:40:59 +0200

    Basic commans change on completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,18 @@ lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({buffer = bufnr})
 end)
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+	['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<Enter>'] = cmp.mapping.confirm({ select = true }),
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
