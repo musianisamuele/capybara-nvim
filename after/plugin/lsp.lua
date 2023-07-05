2bccc46b2febda29c4b1c459271e5e17a923f45f
Musiani Samuele <samuele.musiani.03@gmail.com>
2023-07-05 23:38:07 +0200

    LSP + cmd manual config but all in one file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,22 +1,108 @@
-local lsp = require('lsp-zero').preset({})
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
 
-lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
-end)
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-	['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<Enter>'] = cmp.mapping.confirm({ select = true }),
+    vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts) 
+  end
 })
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+require('mason').setup()
+local mason_lspconfig = require("mason-lspconfig").setup({
+	ensure_installed = { 
+		"clangd", 		-- C, C++ 
+		"bashls", 		-- Bash
+		"cmake",  		-- CMake
+		"ansiblels",  		-- Ansible
+		"golangci_lint_ls", 	-- Go
+		"gopls", 	-- Go
+		"jdtls", 		-- Java
+		"texlab",		-- LaTeX
+		"lua_ls",		-- Lua
+		"rust_analyzer", 	-- Rust
+		"yamlls"		-- YAML
+	}
 })
 
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lsp.setup()
+require('mason-lspconfig').setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = lsp_capabilities,
+    })
+  end,
+})
+
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+-- local lsp = require('lsp-zero').preset({})
+--
+-- lsp.on_attach(function(client, bufnr)
+-- 	lsp.default_keymaps({
+--         buffer = bufnr,
+--         omit = {'<F2>'},
+--         vim.keymap.set('n', '<Space>r', '<cmd>lua vim.lsp.buf.rename()<cr>', {buffer = true})
+--     })
+-- end)
+--
+-- local cmp = require('cmp')
+-- local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+-- 	['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+-- 	['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+-- 	['<Enter>'] = cmp.mapping.confirm({ select = true }),
+-- })
+--
+-- lsp.setup_nvim_cmp({
+-- 	mapping = cmp_mappings
+-- })
+--
+-- -- (Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+--
+-- lsp.setup()
--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,17 +1,17 @@
-local mason = require("mason").setup()
-
-local mason_lspconfig = require("mason-lspconfig").setup({
-	ensure_installed = { 
-		"clangd", 		-- C, C++ 
-		"bashls", 		-- Bash
-		"cmake",  		-- CMake
-		"ansiblels",  		-- Ansible
-		"golangci_lint_ls", 	-- Go
-		"gopls", 	-- Go
-		"jdtls", 		-- Java
-		"texlab",		-- LaTeX
-		"lua_ls",		-- Lua
-		"rust_analyzer", 	-- Rust
-		"yamlls"		-- YAML
-	}
-})
+-- local mason = require("mason").setup()
+--
+-- local mason_lspconfig = require("mason-lspconfig").setup({
+-- 	ensure_installed = { 
+-- 		"clangd", 		-- C, C++ 
+-- 		"bashls", 		-- Bash
+-- 		"cmake",  		-- CMake
+-- 		"ansiblels",  		-- Ansible
+-- 		"golangci_lint_ls", 	-- Go
+-- 		"gopls", 	-- Go
+-- 		"jdtls", 		-- Java
+-- 		"texlab",		-- LaTeX
+-- 		"lua_ls",		-- Lua
+-- 		"rust_analyzer", 	-- Rust
+-- 		"yamlls"		-- YAML
+-- 	}
+-- })
