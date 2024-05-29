880fafdc67ad488a784911455cf7f96a0ed736d7
Samuele Musiani <samu@teapot.ovh>
2024-05-29 19:27:42 +0200

    removed gopls as predefined

--- lua/capybara/lazy.lua
+++ lua/capybara/lazy.lua
@@ -178,7 +178,7 @@ require("lazy").setup({
 			-- Enable the following language servers
 			local servers = {
 				clangd = {},
-				gopls = {},
+				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
